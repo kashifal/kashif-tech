be6508445261b9eb06cfda1e3c2bd90d8ecec868
Kashif Sulehria <captainkashi4@gmail.com>
2024-03-21 05:22:36 +0500

    Home page - resme deleted

--- src/App.jsx
+++ src/App.jsx
@@ -50,29 +50,6 @@ function App() {
           <Socials />
 
           <Footer />
-          <a
-            href={kashif}
-            download
-            className="fixed h-10 flex items-center justify-center border border-gray-600 w-10  sm:w-auto sm:px-4  bottom-8 rounded-full right-8"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="sm:w-3 mt-1 sm:h-3 h-4 w-4 text-gray-600 animate-bounce"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
-              />
-            </svg>
-            <h1 className="text-gray-600 sm:inline-flex hidden text-sm font-mono px-2">
-              Resume
-            </h1>
-          </a>
         </Wrap>
       </div>
     </>
